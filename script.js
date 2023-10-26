ec8759e343ce3eb490e5ec13b11a454d0c476da7
Mahfuza <ibragimovamahfuza464@gmail.com>
2023-10-27 00:59:14 +0500

    added

--- script.js
+++ script.js
@@ -290,19 +290,3 @@ const slider = function () {
 slider();
 
 
-
-document.addEventListener("keydown", function (event) {
-  if (event.ctrlKey) {
-    event.preventDefault();
-  }
-
-  if (event.keyCode == 123) {
-    event.preventDefault();
-  }
-});
-
-document.addEventListener(
-  "contextmenu",
-
-  (event) => event.preventDefault()
-);
